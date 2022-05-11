743ae8dace7ea6ca3e7c3d953b833738a6070771
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-11 09:15:09 -0400

    render stocks to DOM

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
+
+const API = 'http://localhost:3001/stocks'
+
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+
+useEffect(()=>{
+ fetch(API)
+ .then((r)=>r.json())
+ .then((stockData)=> setStocks(stockData))
+  },[])
+
+  
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
